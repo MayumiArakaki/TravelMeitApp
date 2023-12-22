6e4972ad8f40212a38f326ce622a19e99148b643
Mayumi <mayumi.arakakisiccha@gmail.com>
2023-12-22 04:23:03 -0500

    feat(detailview): BTNSEARCH add btn search, set up detail view

--- Pods/Target Support Files/Home/Home-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Home : NSObject
-@end
-@implementation PodsDummy_Home
-@end
--- Pods/Target Support Files/Pods-TravelMeit/Pods-TravelMeit-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TravelMeit : NSObject
-@end
-@implementation PodsDummy_Pods_TravelMeit
-@end
